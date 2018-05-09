cb5befb6d9aee07a9a1261a674c1a2bd21cdefdb
Robert Jaworowski <robert.jaworowski@interia.pl>
2018-05-09 18:33:15 +0200

    first commit

--- src/pl/sdacademy/todoapp/GuessNumber.java
+++
@@ -1,4 +0,0 @@
-package pl.sdacademy.todoapp;
-
-public class GuessNumber {
-}
--- src/pl/sdacademy/todoapp/Main.java
+++
@@ -1,8 +0,0 @@
-package pl.sdacademy.todoapp;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
